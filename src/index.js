10c5afa46e490fc1a90f37c38b24b1f9356749c4
James Earl Douglas <james@earldouglas.com>
2025-07-20 20:10:04 -0700

    Move Nightscout-specific treatment schema from glooko-client

--- src/index.js
+++ src/index.js
@@ -11,31 +11,74 @@ const nightscoutApiSecret = env.envVarReq('NIGHTSCOUT_API_SECRET');
 async function main() {
 
   const glookoCookie =
-    await glookoClient
-    .getGlookoCookie(
-      glookoEmail,
-      glookoPassword
-    );
+    await
+      glookoClient
+        .getGlookoCookie(
+          glookoEmail,
+          glookoPassword
+        );
 
   const glookoCode =
-    await glookoClient
-    .getGlookoCode(
-      glookoCookie
-    );
+    await
+      glookoClient
+        .getGlookoCode(
+          glookoCookie
+        );
 
   const boluses =
-    await glookoClient
-    .getBoluses(
-      glookoCookie,
-      glookoCode
-    );
+    await
+      glookoClient
+        .getBoluses(
+          glookoCookie,
+          glookoCode
+        ).then((boluses) =>
+          boluses
+            .normalBoluses
+            .map((x) => {
+
+              const localDate = (timestampISOString, offsetString) => {
+
+                const offsetRegex = /^([+-])(\d\d):(\d\d)$/;
+
+                // HACK: fix the incorrect time zone provided by Glooko
+                const offsetGroups =
+                  offsetString === "+00:00" ?
+                  "-07:00".match(offsetRegex) :
+                  offsetString.match(offsetRegex);
+
+                const offsetSign = offsetGroups[1] === '+' ? -1 : 1;
+                const offsetHours = Number(`${offsetGroups[2]}`)
+                const offsetMins = Number(`${offsetGroups[3]}`)
+
+                const totalOffsetInMillis =
+                  offsetSign * (offsetHours * 60 + offsetMins) * 60 * 1000;
+
+                const offsetDate = new Date(timestampISOString);
+                offsetDate.setTime(offsetDate.getTime() + totalOffsetInMillis);
+
+                return offsetDate;
+              };
+
+              const timestamp =
+                localDate(x.pumpTimestamp, x.pumpTimestampUtcOffset)
+                .toISOString();
+
+              return {
+                eventType: 'Bolus',
+                insulin: x.insulinDelivered,
+                'created_at': timestamp,
+              };
+            })
+        );
 
   const done =
-    await nightscoutClient.addTreatments(
-      nightscoutHost,
-      nightscoutApiSecret,
-      boluses
-    );
+    await
+      nightscoutClient
+        .addTreatments(
+          nightscoutHost,
+          nightscoutApiSecret,
+          boluses
+        );
 
   return done;
 };
